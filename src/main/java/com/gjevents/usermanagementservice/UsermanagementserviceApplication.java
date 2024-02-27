0e2394158bbf834b6557068eb77078beed9330b7
Abdrahemene <abdrahemenejouini@gmail.com>
2024-02-27 11:27:56 +0100

    first commit

---
+++ src/main/java/com/gjevents/usermanagementservice/UsermanagementserviceApplication.java
@@ -0,0 +1,13 @@
+package com.gjevents.usermanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsermanagementserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsermanagementserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gjevents/usermanagementservice/UsermanagementserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gjevents.usermanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
